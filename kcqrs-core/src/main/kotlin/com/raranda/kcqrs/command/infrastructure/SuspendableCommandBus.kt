9b3e618a77c0a028b5ff55cfc43028ef9abaaf23
Ramon Aranda <ramon.aranda.r@gmail.com>
2018-11-05 19:01:20 +0100

    Modularize project

--- kcqrs-core/src/main/kotlin/com/raranda/kcqrs/command/infrastructure/SuspendableCommandBus.kt
+++ kcqrs-core/src/main/kotlin/com/raranda/kcqrs/command/infrastructure/SuspendableCommandBus.kt
@@ -4,13 +4,14 @@ import com.raranda.kcqrs.command.domain.Command
 import com.raranda.kcqrs.command.domain.CommandBus
 import com.raranda.kcqrs.command.domain.CommandHandler
 import com.raranda.kcqrs.shared.NoRegisteredHandlerException
-import kotlinx.coroutines.experimental.DefaultDispatcher
-import kotlinx.coroutines.experimental.runBlocking
-import kotlinx.coroutines.experimental.withContext
-import kotlin.coroutines.experimental.CoroutineContext
+import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import java.util.concurrent.Executors
+import kotlin.coroutines.CoroutineContext
 import kotlin.reflect.KClass
 
-class SuspendableCommandBus(private val customCoroutineContext: CoroutineContext = DefaultDispatcher) : CommandBus {
+class SuspendableCommandBus(private val customCoroutineContext: CoroutineContext = Executors.newFixedThreadPool(10).asCoroutineDispatcher()) : CommandBus {
     private val handlers: MutableMap<KClass<*>, CommandHandler<*, *>> = mutableMapOf()
 
     override fun <C : Command> register(handler: CommandHandler<C, *>) {
--- kcqrs-core/src/main/kotlin/com/raranda/kcqrs/query/infrastructure/SuspendableQueryBus.kt
+++ kcqrs-core/src/main/kotlin/com/raranda/kcqrs/query/infrastructure/SuspendableQueryBus.kt
@@ -4,13 +4,14 @@ import com.raranda.kcqrs.query.domain.Query
 import com.raranda.kcqrs.query.domain.QueryBus
 import com.raranda.kcqrs.query.domain.QueryHandler
 import com.raranda.kcqrs.shared.NoRegisteredHandlerException
-import kotlinx.coroutines.experimental.DefaultDispatcher
-import kotlinx.coroutines.experimental.runBlocking
-import kotlinx.coroutines.experimental.withContext
-import kotlin.coroutines.experimental.CoroutineContext
+import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import java.util.concurrent.Executors
+import kotlin.coroutines.CoroutineContext
 import kotlin.reflect.KClass
 
-class SuspendableQueryBus(private val customCoroutineContext: CoroutineContext = DefaultDispatcher) : QueryBus {
+class SuspendableQueryBus(private val customCoroutineContext: CoroutineContext = Executors.newFixedThreadPool(10).asCoroutineDispatcher()) : QueryBus {
     private val handlers: MutableMap<KClass<*>, QueryHandler<*, *>> = mutableMapOf()
 
     override fun <Q : Query> register(handler: QueryHandler<Q, *>) {
